a7fdb7747b617c44def4dd2bedd0651526f74b49
Sanchit Gumber <sanchitgumber@Sanchits-Laptop.local>
2024-09-09 04:53:28 +0530

    for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/2048-in-react",
+    basePath: "/chewing_timer",
     output: "export",
     reactStrictMode: true,
   
